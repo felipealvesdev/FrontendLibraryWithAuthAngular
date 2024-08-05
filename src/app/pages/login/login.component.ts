1feb6b9af944c3808a94838a0fe71bd22d5f5850
Felipe Alves <felipealexandre1408@gmail.com>
2024-08-05 16:59:54 -0300

    Feat: Home route has been added to the application and navbar component has been created.

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { InputLoginComponent } from "../../components/input-login/input-login.component";
+import { RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [InputLoginComponent],
+  imports: [InputLoginComponent, RouterOutlet, RouterModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
 import { InputLoginComponent } from "../../components/input-login/input-login.component";
+import { Router, RouterModule, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [InputLoginComponent],
+  imports: [InputLoginComponent, RouterOutlet, RouterModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-
 }
