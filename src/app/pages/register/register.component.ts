2fb83a6eb72bc495d117e05ed8e8f0496dc2b442
Felipe Alves <felipealexandre1408@gmail.com>
2024-08-15 16:14:05 -0300

    Feat: Register route is sending post request sucessfully.

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { InputLoginComponent } from "../../components/input-login/input-login.component";
 import { Router, RouterModule, RouterOutlet } from '@angular/router';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [InputLoginComponent, RouterOutlet, RouterModule],
+  imports: [InputLoginComponent, RouterOutlet, RouterModule, HttpClientModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
-
   loginValue: string = '';
   passwordValue: string = '';
+  userTypeValue: string = "ADMIN" || "USER";
+  resetInputs: boolean = false;
+
+
+  constructor(private http: HttpClient) {}
 
   onLoginValueChange(value: string) {
     this.loginValue = value;
@@ -22,8 +27,34 @@ export class RegisterComponent {
     this.passwordValue = value;
   }
 
+  onUserTypeValueChange(value: string) {
+    this.userTypeValue = value.toUpperCase();
+  }
+
   onSubmit() {
     console.log(`Login: ${this.loginValue}`);
-    console.log(`Password ${this.passwordValue}`)
+    console.log(`Password: ${this.passwordValue}`)
+    console.log(`Role: ${this.userTypeValue}`)
+
+    const loginData = {
+      login: this.loginValue,
+      password: this.passwordValue,
+      role: this.userTypeValue,
+    };
+
+    this.http.post('http://localhost:8080/auth/register', loginData)
+      .subscribe(
+        response => {
+          console.log("Registro bem-sucedido: (backend retorna 200 e body null)", response);
+        },
+        error => {
+          console.log("Erro ao fazer registro: ", error);
+        }
+      );
+
+
+    this.resetInputs = true;
+    setTimeout(() => this.resetInputs = false, 0);
+
   }
 }
