c28b20bd0d8d6224b07c0f30dad3261b7fe889db
Felipe Alves <felipealexandre1408@gmail.com>
2024-08-28 15:44:46 -0300

    Feat: The token returned from login is being stored into the state successfully and being shown on the console.log on the home page.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,8 +1,12 @@
+import { map } from 'rxjs/operators';
+import { selectToken } from './../../state/auth/auth.selectors';
 import { LibraryDataService } from '../../services/library-data/library-data.service';
 import { Component } from '@angular/core';
 import { NavbarComponent } from "../../components/navbar/navbar.component";
 import { CardItemMenuComponent } from "../../components/card-item-menu/card-item-menu.component";
 import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { Store } from '@ngrx/store';
+import { AuthState } from '../../state/auth/auth.reducer';
 
 @Component({
   selector: 'app-home',
@@ -18,11 +22,16 @@ export class HomeComponent {
 
   constructor(
     private LibraryDataService: LibraryDataService,
-    private http: HttpClient
+    private http: HttpClient,
+    private store: Store<AuthState>
   ) {}
 
   ngOnInit(): void {
     this.loadBooks();
+    this.store.select(selectToken).subscribe(state => {
+      console.log(`O valor do token é: ${state}`)
+      // token is being stored in the selector
+    })
   }
 
   loadBooks(): void {
